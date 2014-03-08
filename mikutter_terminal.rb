4c59e1d27289e939f4e1978173ca31a2ba1cfb73
Rhenium <rhenium@rhe.jp>
2014-03-08 21:53:29 +0900

    refactor

--- mikutter_terminal.rb
+++ mikutter_terminal.rb
@@ -2,19 +2,35 @@ require "gtk2"
 require "vte"
 
 Plugin.create :mikutter_terminal do
+  terminal_box_class = Class.new(Gtk::HBox) do
+    attr_reader :terminal, :scrollbar
+    def initialize
+      super
+
+      @terminal = Vte::Terminal.new
+      @terminal.fork_command(argv: [ENV["SHELL"] || "sh"])
+
+      @scrollbar = Gtk::VScrollbar.new(@terminal.adjustment)
+
+      self.add(@terminal).add(@scrollbar)
+    end
+
+    def active
+      get_ancestor(Gtk::Window).set_focus(@terminal) if get_ancestor(Gtk::Window)
+    end
+  end
+
   command(:open_terminal,
           name: "端末を開く",
           condition: ->_ { true },
           role: :pane) do
-    terminal = Vte::Terminal.new
-    terminal.fork_command(argv: [ENV["SHELL"] || "sh"])
-    scroll = Gtk::VScrollbar.new(terminal.adjustment)
+    box = terminal_box_class.new
 
     tab(:terminal, "端") do
-      terminal.signal_connect("child-exited") { self.destroy }
+      box.terminal.signal_connect("child-exited") { self.destroy }
 
       temporary_tab
-      nativewidget Gtk::HBox.new.add(terminal).add(scroll)
+      nativewidget box
       active!
     end
   end
