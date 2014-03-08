1d6d8e4e1d03afd9eeaef7f6865557fd414c5d86
Rhenium <rhenium@rhe.jp>
2014-03-08 21:53:52 +0900

    add feature: change terminal font

--- mikutter_terminal.rb
+++ mikutter_terminal.rb
@@ -2,12 +2,15 @@ require "gtk2"
 require "vte"
 
 Plugin.create :mikutter_terminal do
+  UserConfig[:mikutter_terminal_font] ||= "monospace 12"
+
   terminal_box_class = Class.new(Gtk::HBox) do
     attr_reader :terminal, :scrollbar
     def initialize
       super
 
       @terminal = Vte::Terminal.new
+      @terminal.set_font(UserConfig[:mikutter_terminal_font])
       @terminal.fork_command(argv: [ENV["SHELL"] || "sh"])
 
       @scrollbar = Gtk::VScrollbar.new(@terminal.adjustment)
@@ -33,5 +36,9 @@ Plugin.create :mikutter_terminal do
       nativewidget box
       active!
     end
+  end
+
+  settings "端末" do
+    font "フォント", :mikutter_terminal_font
   end
 end
